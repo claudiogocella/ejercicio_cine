c109779337bf5ba9fe1f48a7bd06801be646bbba
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-05-20 21:50:21 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+//CINE (hacer un a funcion)
+let cine : boolean = [true, false false, true]
+  //1 - CREAR EL ARREGLO CON LOS VALORES LOGICOS (true, false, false, true)
+  //2 - crear las variables sillas desocupadas 
+  //3 - si la silla esta desocupada (false) entonces sillas desocupadas +1 
+  //4 - retornar el valor de las sillas desocupadas 
+
+
+  // multiplicacion 
+/*
+  arreglo1
+  arreglo2
+  arreglo3 new array (3)
+
+  function multiplicarArreglo (arreglo1, arreglo2, arreglo3) {
+    for { 
+      arreglo3[indice] = arreglo1[indice] * arreglo2[]
+    } 
+      
+  } 
+  */
